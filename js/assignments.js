294d07ec086790a6703f59a526e4ead4ed9489d5
EL-MORREL <radek@el-morrel.cz>
2026-05-25 18:38:15 +0200

    Remove vehicle assignment handling from assignments.js
    Removed vehicle assignment logic and related checks.

--- js/assignments.js
+++ js/assignments.js
@@ -145,47 +145,9 @@ if(hasAbsence){
     const w = workerById(rowId);
     if(w && !workerHasSkill(w, job.skill)){
       setStatus("Upozornění: pracovník nemá požadovanou specializaci");}}
-if(rowKind === "vehicle"){
-
-  const duplicateVehicle =
-    db.assignments.some(x =>
-      Number(x.vehicleId) === Number(rowId) &&
-      x.date === date &&
-      Number(x.jobId) !== Number(jobId)
-    );
-
-  a.vehicleId = rowId;
-
-  db.assignments.forEach(x => {
-    if(
-      Number(x.jobId) === Number(a.jobId) &&
-      x.date === a.date
-    ){
-      x.vehicleId = a.vehicleId;
-      x.vehicleLoad = a.vehicleLoad;
-    }
-  });
-
-  if(duplicateVehicle){
-    alert(
+alert(
   "⚠ Vozidlo je již použito na jiné zakázce"
 );
-  }
-
-  const vehicle = vehicleById(rowId);
-
-  const crew =
-    vehicleCrewCount(rowId, date);
-
-  if(
-    vehicle &&
-    crew > (vehicle.peopleCapacity || 5)
-  ){
-    setStatus(
-      "⚠ Překročena kapacita vozidla"
-    );
-  }
-}
   if(job.state === "Nová"){
     job.state = "Naplánováno";}
   await saveDb();
