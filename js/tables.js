748bb28972ce80a7b872e5f36b67164adb8b4b26
EL-MORREL <radek@el-morrel.cz>
2026-06-01 11:21:50 +0200

    Add functions to load and save jobs table

---
+++ js/tables.js
@@ -0,0 +1,27 @@
+async function loadJobsTable(){
+
+  const { data, error } = await supabaseClient
+    .from("jobs")
+    .select("*");
+
+  if(error){
+    console.error(error);
+    return [];
+  }
+
+  return data || [];
+}
+
+async function saveJobTable(job){
+
+  const { error } = await supabaseClient
+    .from("jobs")
+    .upsert(job);
+
+  if(error){
+    console.error(error);
+    throw error;
+  }
+
+  return true;
+}
