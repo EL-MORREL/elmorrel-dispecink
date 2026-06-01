16adcb275a6c534a8d655133bcd5ec104207a7ea
EL-MORREL <radek@el-morrel.cz>
2026-06-01 13:11:30 +0200

    Update assignments.js

--- js/assignments.js
+++ js/assignments.js
@@ -438,7 +438,7 @@ async function saveAssignmentFromModal(){
 
 closeModal("assignModal");
 render();
-
+}
 async function unassignCurrent(){
   if(!canEdit){
     alert("Nemáte oprávnění k úpravám");
@@ -455,7 +455,7 @@ await deleteAssignmentTable(
 
 closeModal("assignModal");
 render();
-
+}
 async function addVehicleAbsence(
   vehicleId,
   date
