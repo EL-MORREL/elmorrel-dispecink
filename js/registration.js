3db2c46626341177436442ae04f4ba0d0688b5ad
EL-MORREL <radek@el-morrel.cz>
2026-09-11 14:49:25 +0200

    Add files via upload

--- js/registration.js
+++ js/registration.js
@@ -1,5 +1,5 @@
-import {client} from './connection.js?v=vacation-1';
-import {esc} from './data.js?v=vacation-1';
+import {client} from './connection.js?v=delete-worker-1';
+import {esc} from './data.js?v=delete-worker-1';
 export function installRegistration(){
  const panel=document.getElementById('login-panel'),login=document.getElementById('login-form');
  const button=document.createElement('button');button.type='button';button.className='secondary';button.id='register-company';button.textContent='Zaregistrovat novou firmu';login.append(button);
